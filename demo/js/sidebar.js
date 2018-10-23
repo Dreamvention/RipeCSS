6813cccfd4ca5f446b518f3f812b70b046267091
dmitriyzhuk <dmitriyzhuk@gmail.com>
2018-10-23 00:46:32 -0400

    initial commit

---
+++ demo/js/sidebar.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    $(document).on("scroll", onScroll);
+
+    //smoothscroll
+    $('a[href^="#"]').on('click', function(e) {
+        e.preventDefault();
+        $(document).off("scroll");
+
+        $('a').each(function() {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top + 2
+        }, 500, 'swing', function() {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+});
+
+function onScroll(event) {
+    var scrollPos = $(document).scrollTop();
+    $('nav a').each(function() {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('nav ul li a').removeClass("active");
+            currLink.addClass("active");
+        }
+    });
+}
---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+var path = require("path");
+var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var concat = require("gulp-concat");
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('autoprefixer');
+var postcssSass = require('@csstools/postcss-sass');
+var browserSync = require('browser-sync');
+
+var baseDir = path.join(__dirname, '/');
+
+if (typeof process.env.HOST === "undefined") {
+    process.env.HOST = 'localhost/ripecss';
+}
+
+gulp.task('css', function() {
+    var processors = [
+        autoprefixer(),
+        postcssSass()
+    ];
+
+    return gulp.src(path.join(baseDir, 'src/css/ripe.scss'))
+        .pipe(sourcemaps.init())
+        .pipe(concat('ripe.css'))
+        .pipe(postcss(processors)).on('error', (e) => console.log(e.message))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(path.join(baseDir, 'dist/css/')))
+        .pipe(browserSync.stream({ match: '**/*.css' }));
+});
+
+
+gulp.task('watch:css', function() {
+    var cssFiles = [];
+    cssFiles.push(path.join(baseDir, '/src/css/scss/', '*.*css'));
+    gulp.watch([cssFiles], ['css']);
+});
+
+gulp.task("serve", function() {
+    if (typeof process.env.HOST !== "undefined") {
+        browserSync({
+            proxy: process.env.HOST
+        });
+    }
+});
+
+gulp.task('default', ["serve"], function() {
+    gulp.start(["watch:css"]);
+});
